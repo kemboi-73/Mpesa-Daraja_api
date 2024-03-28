0bb1b4853fe2abcbe687f9bfce44fae3c0e78892
Kemboi-73 <you@example.com>
2024-03-28 14:26:21 +0300

    urls redirection

--- MpesaDarajaapi/urls.py
+++ MpesaDarajaapi/urls.py
@@ -6,13 +6,8 @@ from . import views
 urlpatterns = [
     path('main/', views.main , name='main'),
     path('accesstoken/', views.find_acesstoken, name='find_acesstoken'),
-    path('initiate/', views.start, name='start'),
-    path('query/', views.run_query, name='query_stk_status'),
-    # path('pay/', views.pay, name='pay'),
-
-    # # # path('', views.home, name="home"),
-    # # path('token', views.token, name='token'),
- 
-    # # path('stk', views.stk, name="stk")
+    path('stk/', views.start, name='start'),
+    path('results/', views.run_query, name='run_query'),
+   
 
 ]
