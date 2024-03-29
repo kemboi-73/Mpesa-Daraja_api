3c17fa473c4ec89e83140ec8fffe78958136adf9
Kemboi Mike <123319720+kemboi-73@users.noreply.github.com>
2024-03-29 20:44:20 +0300

    acess url link up

--- MpesaDarajaapi/acesstokener.py
+++ MpesaDarajaapi/acesstokener.py
@@ -1,15 +1,21 @@
 import requests
 from django.http import JsonResponse
 
+# generates an acesss token from safaricom servers
+
 def find_acesstoken(request):
-    consumer_key = "6LyDoxG9yJ9BTvRi0Cl9fKJRGoXFZNNx"  # Fill with your app Consumer Key
+    consumer_key = "6LyDoxG9yJ9BTvRi0Cl9fKJRGoXFZNNx"  # app Consumer Key from safaricom
     consumer_secret = "NdXlNAEiFZWcYAjb"  # app Consumer Secret
     access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    
+    # Response
+    
     headers = {'Content-Type': 'application/json'}
     auth = (consumer_key, consumer_secret)
     try:
         response = requests.get(access_token_url, headers=headers, auth=auth)
-        response.raise_for_status()  # Raise exception for non-2xx status codes
+        # Raise exception for non-2xx status codes
+        response.raise_for_status()  
         result = response.json()
         access_token = result['access_token']
         return JsonResponse({'access_token': access_token})
