76d1c206e2e3fcfb9fbaf69407ec4e5b7feefc1e
Kemboi Mike <123319720+kemboi-73@users.noreply.github.com>
2024-03-30 09:38:32 +0300

    acess_token_show

--- MpesaDarajaapi/urls.py
+++ MpesaDarajaapi/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 # from .stkPush import pay  
-
+# from .views import payment_list # reedirect to your views.py
 urlpatterns = [
     path('main/', views.main , name='main'),
     path('accesstoken/', views.find_acesstoken, name='find_acesstoken'), #Retriving and seeing the current acess token
     path('stk/', views.start, name='start'), #for initiating the process
     path('results/', views.run_query, name='run_query'), # for query running
-   
-
+    path('admin/', admin.site.urls),  
+    # path('payments/', payment_list, name='payment_list'), #redirect the list to the template you want
+    
 ]
