679a28980c63a703d81d505a04e678d585041080
Kemboi Mike <123319720+kemboi-73@users.noreply.github.com>
2024-03-30 09:42:46 +0300

    runquery views implementation

--- MpesaDarajaapi/views.py
+++ MpesaDarajaapi/views.py
@@ -6,3 +6,11 @@ from .main import main
 from .acesstokener import find_acesstoken
 from .stkPush import start
 from .query import run_query
+
+
+#Update for frontend payment list 
+# from .models import Payment
+
+# def payment_list(request):
+#     payments = Payment.objects.all()
+#     return render(request, 'payment_list.html', {'payments': payments})
