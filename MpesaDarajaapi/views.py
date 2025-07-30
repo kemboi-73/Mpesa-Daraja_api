577bb7daf2f93eb36c9a80b757b8674ab7d0bc34
kemboi-73 <kemboimichael52@gmail.com>
2025-07-30 21:04:08 +0300

    `Updated stkform.html template to change text color and added python-dotenv to requirements.txt`

--- MpesaDarajaapi/views.py
+++ MpesaDarajaapi/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
-
-
-# Create your views here.
 from .main import main
 from .acesstokener import find_acesstoken
 from .stkPush import start
