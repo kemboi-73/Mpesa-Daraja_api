cd177ee024d333af785c2a195118655428a943a9
kemboi-73 <kemboimichael52@gmail.com>
2025-07-30 19:25:46 +0300

    `Removed unused URL patterns `

--- MpesaDarajaapi/urls.py
+++ MpesaDarajaapi/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 from .views import stk_form_view
 from .callback import process_stk_callback 
+
 urlpatterns = [
-    path('main/', views.main , name='main'),
-    path('accesstoken/', views.find_acesstoken, name='find_acesstoken'), #Retriving and seeing the current acess token
-    path('stk/', views.start, name='start'), #for initiating the process
-    path('results/', views.run_query, name='run_query'), # for query running
+    # path('main/', views.main , name='main'),
+    # path('accesstoken/', views.find_acesstoken, name='find_acesstoken'), #Retriving and seeing the current acess token
+    # path('stk/', views.start, name='start'), #for initiating the process
+    # path('results/', views.run_query, name='run_query'), # for query running
     path('admin/', admin.site.urls),  
     # path('payments/', payment_list, name='payment_list'), #redirect the list to the template you want
     path('', stk_form_view, name='stk_form'),
