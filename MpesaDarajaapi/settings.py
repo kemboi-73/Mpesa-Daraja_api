689d43ab1e25b31c7c55aaced598fbe034abf10d
kemboi-73 <kemboimichael52@gmail.com>
2025-07-30 18:19:33 +0300

    "Updated requirements.txt with additional dependencies: gunicorn and packaging."

--- MpesaDarajaapi/settings.py
+++ MpesaDarajaapi/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-7ar)$h-6v3a*f3&r(4ygi*^bnvi)p%l^+j4t_+@y9p)iec!em-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mpesa-i42i.onrender.com/']
 
 
 # Application definition
