0b9818535ad6077904747152aec82ca6caa3da1f
kemboi-73 <kemboimichael52@gmail.com>
2025-08-06 22:28:18 +0300

    `Updated static files and CSS styles in MpesaDarajaapi project.`

--- MpesaDarajaapi/settings.py
+++ MpesaDarajaapi/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # ✅ this is for development static files
+     os.path.join(BASE_DIR, 'MpesaDarajaapi', 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
